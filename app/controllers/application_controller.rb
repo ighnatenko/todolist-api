64e5687e1cd9362e69ec72508eae409bbded6abb
ighnatenko <ighnatenko@meta.ua>
2018-06-03 02:49:13 +0300

    add cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { render nothing: true, :status => :forbidden }
+    end
+  end
+
+  def current_ability
+    @current_ability = Ability.new(current_api_user)
+  end
 end
