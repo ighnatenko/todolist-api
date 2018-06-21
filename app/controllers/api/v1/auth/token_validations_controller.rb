2410cf2c31ae7f2536918219fe16fad747a2ad7c
ighnatenko <ighnatenko@meta.ua>
2018-06-21 23:05:41 +0300

    refactoring

--- app/controllers/api/v1/auth/token_validations_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::Auth::TokenValidationsController < DeviseTokenAuth::TokenValidationsController
-  include SetCacheControlHeader
-  
-  def render_validate_token_success
-    render json: @resource
-  end
-end
